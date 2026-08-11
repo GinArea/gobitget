8d9d4c61b4445157f5f1e00ac9a043436215fd74
Oleg Kuzmenko <>
2026-08-11 13:41:59 +0300

    Private mthods added

--- bitgetv3/account.go
+++ bitgetv3/account.go
@@ -40,3 +40,95 @@ type AccountInfo struct {
 	// RegisTime - Account registration time (Unix timestamp in milliseconds)
 	RegisTime ujson.Int64
 }
+
+// GetDepositAddress - request for GET /api/v3/account/deposit-address (UTA)
+// https://www.bitget.com/api-doc/uta/account/deposit/Get-Deposit-Address
+// Rate limit: 10/sec/UID, permission: UTA mgt. (read)
+type GetDepositAddress struct {
+	// Coin - Coin name, e.g. USDT
+	Coin string
+	// Chain - Chain name, e.g. trc20 (auto-matched if omitted)
+	Chain string `url:",omitempty"`
+	// Size - Deposit quantity, for BTC Lightning Network only, range: 0.000001 - 0.001
+	Size string `url:",omitempty"`
+}
+
+func (o GetDepositAddress) Do(c *Client) Response[DepositAddress] {
+	return Get(c, "account/deposit-address", o, forward[DepositAddress])
+}
+
+func (o *Client) GetDepositAddress(v GetDepositAddress) Response[DepositAddress] {
+	return v.Do(o)
+}
+
+// DepositAddress - response for GET /api/v3/account/deposit-address (UTA)
+// https://www.bitget.com/api-doc/uta/account/deposit/Get-Deposit-Address
+type DepositAddress struct {
+	// Address - Deposit address
+	Address string
+	// Chain - Chain name
+	Chain string
+	// Coin - Coin name
+	Coin string
+	// Tag - Tag (API may return null)
+	Tag string
+	// Url - Blockchain explorer address
+	Url string
+}
+
+// GetDepositRecords - request for GET /api/v3/account/deposit-records (UTA)
+// https://www.bitget.com/api-doc/uta/account/deposit/Get-Deposit-Records
+// Rate limit: 10/sec/UID, permission: UTA mgt. (read)
+type GetDepositRecords struct {
+	// StartTime - Query record start time, unix millisecond timestamp (interval with EndTime cannot be greater than 30 days)
+	StartTime int64
+	// EndTime - Query record end time, unix millisecond timestamp
+	EndTime int64
+	// Coin - Coin name; if left blank, all currency deposit records will be retrieved
+	Coin string `url:",omitempty"`
+	// OrderId - Order ID, used for specifying order queries
+	OrderId string `url:",omitempty"`
+	// Limit - Items per page, default 20, max 100
+	Limit int `url:",omitempty"`
+	// Cursor - Cursor ID for pagination; use the smallest orderId returned from the previous query
+	Cursor string `url:",omitempty"`
+}
+
+func (o GetDepositRecords) Do(c *Client) Response[[]DepositRecord] {
+	return Get(c, "account/deposit-records", o, forward[[]DepositRecord])
+}
+
+func (o *Client) GetDepositRecords(v GetDepositRecords) Response[[]DepositRecord] {
+	return v.Do(o)
+}
+
+// DepositRecord - item of GET /api/v3/account/deposit-records response (UTA)
+// https://www.bitget.com/api-doc/uta/account/deposit/Get-Deposit-Records
+type DepositRecord struct {
+	// OrderId - Order ID
+	OrderId string
+	// RecordId - Deposit record ID: the on-chain hash when Dest is on_chain, the order ID when Dest is internal_transfer
+	RecordId string
+	// ClientOid - Client order ID (not documented, API returns it, may be null)
+	ClientOid string
+	// Coin - Coin name
+	Coin string
+	// Type - Operation type: deposit
+	Type string
+	// Dest - Deposit type: on_chain, internal_transfer
+	Dest string
+	// Size - Deposit quantity
+	Size ujson.Float64
+	// Status - Deposit status: pending, success, fail
+	Status string
+	// FromAddress - Deposit initiator: the on-chain address when Dest is on_chain, the UID/email/mobile when Dest is internal_transfer
+	FromAddress string
+	// ToAddress - Deposit recipient: the on-chain address when Dest is on_chain, the UID/email/mobile when Dest is internal_transfer
+	ToAddress string
+	// Chain - Deposit network (can be ignored when Dest is internal_transfer)
+	Chain string
+	// CreatedTime - Deposit record creation time, unix millisecond timestamp
+	CreatedTime ujson.Int64
+	// UpdatedTime - Deposit record update time, unix millisecond timestamp
+	UpdatedTime ujson.Int64
+}
